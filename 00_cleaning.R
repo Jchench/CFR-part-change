4cd9574602b5629c7e5d7320ccc1b1ef18ee28c4
Jchench <jessiechen2026@u.northwestern.edu>
2024-07-31 10:45:59 -0700

    adding activities

--- 00_cleaning.R
+++ 00_cleaning.R
@@ -7,7 +7,7 @@ replace_values <- function(df, target_column, reference_column) {
   values_to_replace <- c("?", "??")
   
   # Use mutate and case_when to replace values
-  df <- df %>% 
+  df <- df |> 
     mutate(!!sym(target_column) := case_when(
       !!sym(target_column) == "–" ~ NA_character_,
       !!sym(target_column) %in% values_to_replace ~ !!sym(reference_column),
@@ -21,7 +21,7 @@ df <- replace_values(df, "same", "CFR Part")
 
 # categorizing
 df <- 
-  df %>% 
+  df |> 
   mutate(category = case_when(
     str_detect(`Name of the Part`, regex("discounts|advances|open market|purchases|reserves|federal|interest|monetary", ignore_case = TRUE)) ~ "monetary policy",
     str_detect(`Name of the Part`, regex("prudential|regulation|capital|risk|liquidity|supervision|leverage|safety", ignore_case = TRUE)) ~ "prudential regulation",
@@ -30,3 +30,10 @@ df <-
   ))
 
 write_csv(df, file = "stacked_data_cleaned.csv")
+
+df2 <- 
+  df |> 
+  filter(Year >= 1949)
+
+# activity type
+
